1c620024ff9b5e84843941df484fda4b68eaca24
Daltimore <obinnaleo78@gmail.com>
2018-05-11 16:18:58 +0100

    routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Categories from '@/components/Categories'
 import Category from '@/components/Category'
+import login from '@/components/Login'
+import SignUp from '@/components/SignUp'
 
 Vue.use(Router)
 
@@ -9,13 +12,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Categoriez',
+      name: 'Categories',
       component: Categories
     },
     {
       path: '/category/*',
       name: 'Category',
       component: Category
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
